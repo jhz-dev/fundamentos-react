d9e5bca747161d26a7ff42118b847f77ca48d943
jhz-dev <jhzapata.seven@gmail.com>
2020-11-04 16:58:51 -0500

    first react component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+
+// functional component
+const MyComponent = () => {
+  return <p>Hello world</p>;
+};
+
+// class component
+class MyClassComponent extends Component {
+  render() {
+    return <p>Hello world from a class component</p>;
+  }
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <MyComponent />
+      <MyClassComponent />
     </div>
   );
 }
