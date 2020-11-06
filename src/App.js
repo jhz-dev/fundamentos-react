f8a49e801ca7245cf981a0684f449c70b1ea6cf1
jhz-dev <jhzapata.seven@gmail.com>
2020-11-06 12:51:49 -0500

    react props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import "./style.css";
 
+/**
+ * Notes:
+ * props => are read only.
+ *
+ * functional components:
+ * * Just receive as a paremeter the props object
+ * * To set the props to a funtional component with set each attribute
+ * <MyComponent name={name}/>
+ */
+
 // functional component
-const MyComponent = () => {
-  return <p>Hello world</p>;
+const MyComponent = props => {
+  const { name } = props;
+  return <p>{name} says hello world</p>;
 };
 
 // class component
@@ -14,9 +25,11 @@ class MyClassComponent extends Component {
 }
 
 export default function App() {
+  const name = "Jonathan";
+
   return (
     <div>
-      <MyComponent />
+      <MyComponent name={name} />
       <MyClassComponent />
     </div>
   );
