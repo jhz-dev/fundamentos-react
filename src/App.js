39f50988a3f7db50c20f81736dd880f9655982aa
jhz-dev <jhzapata.seven@gmail.com>
2020-11-06 13:06:27 -0500

    jsx

--- src/App.js
+++ src/App.js
@@ -9,16 +9,29 @@ import "./style.css";
  * * Just receive as a paremeter the props object
  * * To set the props to a funtional component with set each attribute
  * <MyComponent name={name}/>
+ *
+ * JSX
+ * Similar syntax to html
+ * for using/calling a component <ComponentName/>
  */
 
 // functional component
 const MyComponent = props => {
-  const { name } = props;
-  return <p>{name} says hello world</p>;
+  const { name, children } = props;
+  return (
+    <div>
+      <p>{name} says hello world</p>
+      {children}
+    </div>
+  );
 };
 
 // class component
 class MyClassComponent extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return <p>Hello world from a class component</p>;
   }
@@ -29,8 +42,10 @@ export default function App() {
 
   return (
     <div>
-      <MyComponent name={name} />
       <MyClassComponent />
+      <MyComponent name={name}>
+        <p>Someone</p>
+      </MyComponent>
     </div>
   );
 }
